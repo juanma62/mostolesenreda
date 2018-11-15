8f43d51ef87d420ce530711a8a04a672358aea4c
juan <juan@iMac-de-juan.local>
2018-11-15 21:00:10 +0100

    Cambios

--- js/altoHeader.js
+++ js/altoHeader.js
@@ -1,12 +1,41 @@
 export class AltoHeader {
     constructor (){
-        this.altoHeader = document.getElementById('headerId').clientHeight
+        this.altoHeader = document.getElementById('headerId').offsetHeight
         this.main = document.getElementById('mainId')
-        window.onload = this.medirAlto()
+        window.addEventListener('resize', this.medirAlto.bind(this))
+        document.onchange = this.medirAlto()
     }
         
     medirAlto(){
-        let altura = this.altoHeader
-        this.main.style.marginTop = altura+"px"
+        if(!this.main.style.marginTop){
+            //location.reload()
+            this.main.style.marginTop = this.altoHeader+'px'
+            console.log('Hola')
+        }else{
+            location.reload(true)
+            console.log('Adios')
+        }
+        /* let altura = 0
+        console.log(altura)
+        fetch(altura, {method: 'GET'})
+        .then(response => {
+            if(!response){
+                console.log('Aquí estamos 1')
+                return response
+                
+            }break
+        }).then(data => this.medirAlto())
+        return new Promise((resolve) => {
+            let altura = this.altoHeader+"px"
+            console.log('Aquí estamos 1')
+        }).then((resolve) => {
+            this.main.style.marginTop = altura
+            console.log('Aquí estamos')
+        })
+        this.main.style.marginTop += this.altoHeader+"px"
+        console.log('Aquí estamos')
+        console.dir(this.main) */
     }
 }
+
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 import { Buscador } from "./buscador.js"
 import { AltoHeader } from "./altoHeader.js";
 
-document.addEventListener('DOMContentLoaded', ()=> {new  Buscador(), new AltoHeader})
+document.addEventListener('DOMContentLoaded', ()=> {new  Buscador(), new AltoHeader()})
