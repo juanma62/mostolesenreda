f4d7db17f5752dd12158d99690243ecea0c6e419
juan <juan@iMac-de-juan.local>
2018-11-15 23:59:56 +0100

    Cambios

--- js/altoHeader.js
+++ js/altoHeader.js
@@ -3,39 +3,17 @@ export class AltoHeader {
         this.altoHeader = document.getElementById('headerId').offsetHeight
         this.main = document.getElementById('mainId')
         window.addEventListener('resize', this.medirAlto.bind(this))
-        document.onchange = this.medirAlto()
+        document.onload = this.medirAlto()
     }
         
     medirAlto(){
         if(!this.main.style.marginTop){
-            //location.reload()
             this.main.style.marginTop = this.altoHeader+'px'
+            //location.reload(false)
             console.log('Hola')
         }else{
-            location.reload(true)
+            location.reload(false)
             console.log('Adios')
         }
-        /* let altura = 0
-        console.log(altura)
-        fetch(altura, {method: 'GET'})
-        .then(response => {
-            if(!response){
-                console.log('Aquí estamos 1')
-                return response
-                
-            }break
-        }).then(data => this.medirAlto())
-        return new Promise((resolve) => {
-            let altura = this.altoHeader+"px"
-            console.log('Aquí estamos 1')
-        }).then((resolve) => {
-            this.main.style.marginTop = altura
-            console.log('Aquí estamos')
-        })
-        this.main.style.marginTop += this.altoHeader+"px"
-        console.log('Aquí estamos')
-        console.dir(this.main) */
     }
 }
-
-
